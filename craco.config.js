b9d9a12170eee0bf01dd29c77ab45502ad6f86dd
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2021-03-17 18:41:05 +0100

    Body background

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,10 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: {'@primary-color': '#1DA57A'},
+            modifyVars: {
+              '@primary-color': '#1DA57A',
+              '@body-background': '@layout-body-background'
+            },
             javascriptEnabled: true
           }
         }
