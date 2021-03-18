85bebef9e415e2f058cb9ef30cc948516b20bed0
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2021-03-18 10:14:22 +0100

    SideMenu style

--- src/components/side-menu/SideMenu.tsx
+++ src/components/side-menu/SideMenu.tsx
@@ -1,4 +1,4 @@
-import {Menu} from 'antd'
+import {Divider, Menu} from 'antd'
 import React from 'react'
 
 export interface MenuEntry {
@@ -9,11 +9,18 @@ export interface SideMenuProps {
   entries: MenuEntry[]
 }
 
+const dividerStyle: React.CSSProperties = {margin: 0, width: '80%', minWidth: '80%', float: 'right'}
+
 export const SideMenu: React.FC<SideMenuProps> = ({entries}) => {
-  const entriesMapper = (entry: MenuEntry) => (<Menu.Item key={entry.name}>{entry.name}</Menu.Item>)
+  const entriesMapper = (entry: MenuEntry) => (
+    <React.Fragment key={entry.name}>
+      <Menu.Item className="menu-entry" key={entry.name}>{entry.name}</Menu.Item>
+      <Divider style={dividerStyle} type="horizontal"/>
+    </React.Fragment>
+  )
 
   return (
-    <Menu mode="inline">
+    <Menu mode="inline" style={{height: '93vh'}}>
       {entries.map(entriesMapper)}
     </Menu>
   )
