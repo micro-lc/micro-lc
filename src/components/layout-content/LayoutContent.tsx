9e992eed64309102b21d116fd75c6be5d23be1f2
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2021-03-19 08:31:49 +0100

    Created AnimatedLayoutSider component

--- src/components/layout-content/LayoutContent.tsx
+++ src/components/layout-content/LayoutContent.tsx
@@ -14,25 +14,41 @@ type LayoutContentProps = PropTypes.InferProps<typeof layoutContentProps>
 export const LayoutContent: React.FC<LayoutContentProps> = ({burgerState: [isOpened, setOpened]}) => {
   const closeSideMenu = () => setOpened(false)
 
-  const variants = {
-    open: {opacity: 1, x: 0},
-    closed: {opacity: 0, x: '-100%'}
-  }
-
   return (
     <Layout>
-      <motion.nav
-        animate={isOpened ? 'open' : 'closed'}
-        transition={{ease: 'linear', duration: 0.2}}
-        variants={variants}
-      >
-        <Layout.Sider width={250}>
-          {isOpened && <SideMenu entries={[{name: 'entry_1'}, {name: 'entry_2'}]}/>}
-        </Layout.Sider>
-      </motion.nav>
+      <AnimatedLayoutSider isOpened={isOpened}/>
       <Layout.Content data-testid="layout-content-overlay" onClick={closeSideMenu}/>
     </Layout>
   )
 }
 
 LayoutContent.propTypes = layoutContentProps
+
+const animatedLayoutProps = {
+  isOpened: PropTypes.bool.isRequired
+}
+
+type AnimatedLayoutProps = PropTypes.InferProps<typeof animatedLayoutProps>
+
+const motionNavSettings = {
+  transition: {ease: 'linear', duration: 0.2},
+  variants: {
+    open: {opacity: 1, x: 0},
+    closed: {opacity: 0, x: '-100%'}
+  }
+}
+
+const AnimatedLayoutSider: React.FC<AnimatedLayoutProps> = ({isOpened}) => {
+  return (
+    <motion.nav
+      animate={isOpened ? 'open' : 'closed'}
+      {...motionNavSettings}
+    >
+      <Layout.Sider width={250}>
+        {isOpened && <SideMenu entries={[{name: 'entry_1'}, {name: 'entry_2'}]}/>}
+      </Layout.Sider>
+    </motion.nav>
+  )
+}
+
+AnimatedLayoutSider.propTypes = animatedLayoutProps
