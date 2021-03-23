100a365dfe11035cdf21d59fca507e50b21d303a
iLTeoooD <matteopietro.dazzi@gmail.com>
2021-03-23 10:41:38 +0100

    AppState spread

--- packages/fe-container/src/App.tsx
+++ packages/fe-container/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
     return () => configurationSubscription.unsubscribe()
   }, [])
 
-  return <Launcher configuration={appState.configuration} isLoading={appState.isLoading}/>
+  return <Launcher {...appState}/>
 }
 
 export default App
