47d1dcbb1ec47e73ba1e98401dcf4a70279d97a3
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-04-08 10:39:11 +0200

    Overlay style

--- packages/fe-container/src/components/user-menu-overlay/UserMenuOverlay.tsx
+++ packages/fe-container/src/components/user-menu-overlay/UserMenuOverlay.tsx
@@ -4,14 +4,16 @@ import {FormattedMessage} from 'react-intl'
 
 import {logOutUser} from '@services/microlc/user.service'
 
+import './UserMenuOverlay.less'
+
 export const UserMenuOverlay: React.FC = () => {
   const logOut = useCallback(() => {
     logOutUser().subscribe(() => window.location.reload())
   }, [])
   return (
-    <Menu>
-      <Menu.Item className='userMenu_entry'>
-        <span className='userMenu_logout' onClick={logOut}>
+    <Menu className='userMenuOverlay_body'>
+      <Menu.Item className='userMenuOverlay_entry'>
+        <span className='userMenuOverlay_logout' onClick={logOut}>
           <FormattedMessage id='logout'/>
         </span>
       </Menu.Item>
--- packages/fe-container/src/components/user-menu/UserMenu.tsx
+++ packages/fe-container/src/components/user-menu/UserMenu.tsx
@@ -1,10 +1,12 @@
 import React, {useCallback, useState} from 'react'
 import {Dropdown} from 'antd'
 import {User} from '@mia-platform/core'
+import PropTypes from 'prop-types'
 
-import './UserMenu.less'
 import {DropdownIcon} from '@components/dropdown-icon/DropdownIcon'
 import {UserMenuOverlay} from '@components/user-menu-overlay/UserMenuOverlay'
+
+import './UserMenu.less'
 
 const retrieveUserAvatar = (user: Partial<User>) => {
   const fallbackUrl = `https://eu.ui-avatars.com/api/?name=${user.name || ''}&size=24x24`
@@ -25,10 +27,28 @@ export const UserMenu: React.FC<Partial<User>> = (user) => {
       trigger={['click']}
     >
       <div className='userMenu_container' data-testid='userMenu_container'>
-        <DropdownIcon dropdownOpened={dropdownOpened}/>
-        <span className='userMenu_name'>{user.name}</span>
-        <img alt='Avatar' className='userMenu_avatar' src={retrieveUserAvatar(user)}/>
+        <UserMenuContent dropdownOpened={dropdownOpened} user={user}/>
       </div>
     </Dropdown>
   )
+}
+
+type UserMenuContentProps = {
+  dropdownOpened: boolean,
+  user: Partial<User>
+}
+
+const UserMenuContent: React.FC<UserMenuContentProps> = ({user, dropdownOpened}) => {
+  return (
+    <>
+      <DropdownIcon dropdownOpened={dropdownOpened}/>
+      <span className='userMenu_name'>{user.name}</span>
+      <img alt='Avatar' className='userMenu_avatar' src={retrieveUserAvatar(user)}/>
+    </>
+  )
+}
+
+UserMenuContent.propTypes = {
+  dropdownOpened: PropTypes.bool.isRequired,
+  user: PropTypes.any.isRequired
 }
