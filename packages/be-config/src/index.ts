ab35ee34ead00fd0a22ee0cb6ea7f8aab6812a7e
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-04-08 18:59:24 +0200

    Setup BE

---
+++ packages/be-config/src/index.ts
@@ -0,0 +1,10 @@
+import {DecoratedFastify} from "@mia-platform/custom-plugin-lib";
+const customService = require('@mia-platform/custom-plugin-lib')()
+
+module.exports = customService(async function index(service:DecoratedFastify) {
+
+  /*
+   * Insert your code here.
+   */
+
+})
