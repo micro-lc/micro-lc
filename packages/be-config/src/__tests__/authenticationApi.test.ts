2538e55b251442f3b52678ff341964d587fd308c
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-04-12 11:36:02 +0200

    Authentication API

---
+++ packages/be-config/src/__tests__/authenticationApi.test.ts
@@ -0,0 +1,32 @@
+import path from 'path'
+import fastify from 'fastify'
+import http from 'http'
+import {DecoratedFastify} from '@mia-platform/custom-plugin-lib'
+
+import {authenticationApiHandlerBuilder} from '../authenticationApi'
+
+describe('Authentication api tests', () => {
+  // @ts-ignore
+  const fastifyInstance: DecoratedFastify = {
+    config: {
+      AUTHENTICATION_CONFIGURATION_PATH: path.join(__dirname, 'validAuthenticationConfig.json'),
+    },
+  }
+
+  const replySendMock = jest.fn()
+
+  // @ts-ignore
+  const replyMock: fastify.FastifyReply<http.ServerResponse> = {
+    send: replySendMock,
+  }
+
+  it('Correctly create handler', async() => {
+    const handler = await authenticationApiHandlerBuilder(fastifyInstance)
+    // @ts-ignore
+    handler(undefined, replyMock)
+    expect(replySendMock).toHaveBeenCalledWith({
+      'isAuthNecessary': true,
+      'userInfoUrl': '/api/v1/microlc/user',
+    })
+  })
+})
