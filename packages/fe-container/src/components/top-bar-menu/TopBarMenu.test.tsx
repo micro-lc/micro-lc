5dc92755dced085c1ab71473621d848e8e3e694b
Davide Bianchi <bianchidavide12@gmail.com>
2021-04-30 16:10:30 +0200

    Apply suggestions from code review

--- packages/fe-container/src/components/top-bar-menu/TopBarMenu.test.tsx
+++ packages/fe-container/src/components/top-bar-menu/TopBarMenu.test.tsx
@@ -56,7 +56,7 @@ describe('TopBarMenu tests', () => {
     expect(document.getElementsByClassName('topBarMenu_container').length).toBe(0)
   })
 
-  it('Doesn\'t render even with a plugin but missing menuLocation', () => {
+  it('doesn\'t render even with a plugin but missing menuLocation', () => {
     const configuration = configurationsBuilder(undefined, [plugin])
     RenderWithReactIntl(
       <ConfigurationProvider value={configuration}>
@@ -66,7 +66,7 @@ describe('TopBarMenu tests', () => {
     expect(document.getElementsByClassName('topBarMenu_container').length).toBe(0)
   })
 
-  it('Doesn\'t render with menuLocation but empty plugins list', () => {
+  it('doesn\'t render with menuLocation but empty plugins list', () => {
     const configuration = configurationsBuilder('topBar', [])
     RenderWithReactIntl(
       <ConfigurationProvider value={configuration}>
@@ -76,7 +76,7 @@ describe('TopBarMenu tests', () => {
     expect(document.getElementsByClassName('topBarMenu_container').length).toBe(0)
   })
 
-  it('Doesn\'t render with menuLocation but undefined plugins list', () => {
+  it('doesn\'t render with menuLocation but undefined plugins list', () => {
     const configuration = configurationsBuilder('topBar', undefined)
     RenderWithReactIntl(
       <ConfigurationProvider value={configuration}>
@@ -86,7 +86,7 @@ describe('TopBarMenu tests', () => {
     expect(document.getElementsByClassName('topBarMenu_container').length).toBe(0)
   })
 
-  it('Render with menuLocation and plugins list', () => {
+  it('render with menuLocation and plugins list', () => {
     const {icon, ...rest} = plugin
     const configuration = configurationsBuilder('topBar', [rest])
     RenderWithReactIntl(
@@ -97,7 +97,7 @@ describe('TopBarMenu tests', () => {
     expect(screen.getByText('Href entry')).toBeTruthy()
   })
 
-  it('Click correctly route to new tab', () => {
+  it('click correctly route to new tab', () => {
     window.open = jest.fn()
     const configuration = configurationsBuilder('topBar', [plugin])
     registerPlugin(plugin)
