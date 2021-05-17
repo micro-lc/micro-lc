494bbae53ffc8bfe7a90d1a533f5cbbfca872126
Valerio Evangelisti <valerio.evangelisti@mia-platform.eu>
2021-05-17 09:20:13 +0200

    cleaned some parts and readme changed

--- packages/ui-components/craco.config.js
+++ packages/ui-components/craco.config.js
@@ -1,18 +1,7 @@
-const CracoLessPlugin = require('craco-less')
 const CracoAlias = require('craco-alias')
 
 module.exports = {
   plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true
-          }
-        }
-      }
-    },
     {
       plugin: CracoAlias,
       options: {
