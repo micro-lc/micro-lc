ef361d1e30a7057f1d0c932179a806b6c9c20456
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-10-28 21:07:25 +0200

    Restored mock

--- build_live_example.sh
+++ build_live_example.sh
@@ -10,6 +10,6 @@ mv ./packages/fe-container/build .
 mv ./example/build ./build/react-app
 
 mkdir -p ./build/api/v1/microlc/
-mv ./mock-server/api/v1/microlc/authentication/authentication.json ./build/api/v1/microlc/authentication
-mv ./mock-server/api/v1/microlc/configuration/configuration.json ./build/api/v1/microlc/configuration
-mv ./mock-server/api/v1/microlc/user/user.json ./build/api/v1/microlc/user
+cp ./mock-server/api/v1/microlc/authentication/authentication.json ./build/api/v1/microlc/authentication
+cp ./mock-server/api/v1/microlc/configuration/configuration.json ./build/api/v1/microlc/configuration
+cp ./mock-server/api/v1/microlc/user/user.json ./build/api/v1/microlc/user
