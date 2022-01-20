5564a0c9f2550c051205761b7190a1f639641d44
iLTeoooD <matteo.dazzi@mia-platform.eu>
2022-01-20 16:03:16 +0100

    Move zone to index

--- packages/fe-container/src/utils/plugins/PluginsLoaderFacade.ts
+++ packages/fe-container/src/utils/plugins/PluginsLoaderFacade.ts
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import './zone-env'
-import 'zone.js'
 import {createBrowserHistory} from 'history'
 import {addErrorHandler, registerMicroApps, RegistrableApp, start} from 'qiankun'
 import {InternalPlugin, Plugin, User} from '@mia-platform/core'
--- packages/fe-container/src/utils/plugins/zone-env.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-file
-// @ts-nocheck
-if (typeof global !== 'undefined') {
-  global.__Zone_disable_registerElement = true
-  global.__Zone_disable_customElements = true
-}
-
-export {}
