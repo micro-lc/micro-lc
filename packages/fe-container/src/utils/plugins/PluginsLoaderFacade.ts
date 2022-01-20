34bf13f042b389ed941f06ada52e98faa298949b
iLTeoooD <matteo.dazzi@mia-platform.eu>
2022-01-20 14:25:53 +0100

    Disable zone for WC

--- packages/fe-container/src/utils/plugins/PluginsLoaderFacade.ts
+++ packages/fe-container/src/utils/plugins/PluginsLoaderFacade.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import './zone-env'
 import 'zone.js'
 import {createBrowserHistory} from 'history'
 import {addErrorHandler, registerMicroApps, RegistrableApp, start} from 'qiankun'
---
+++ packages/fe-container/src/utils/plugins/zone-env.ts
@@ -0,0 +1,8 @@
+// eslint-disable-file
+// @ts-nocheck
+if (typeof global !== 'undefined') {
+  global.__Zone_disable_registerElement = true
+  global.__Zone_disable_customElements = true
+}
+
+export {}
