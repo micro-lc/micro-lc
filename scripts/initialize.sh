70bc7de842896e26b7d6e8a6f06b47fefc59de11
Umberto Toniolo <amountainram@gmail.com>
2022-10-05 21:05:52 +0200

    added intialization script

---
+++ scripts/initialize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GREEN="\e[1;32m"
+ENDCOLOR="\e[0m"
+
+echo -e "\n\U0001F5D1${GREEN}\tcleanup${ENDCOLOR}\n"
+yarn cleanup
+
+echo -e "\n\u2699${GREEN}\tinterfaces${ENDCOLOR}\n"
+yarn i check:types
+yarn i build
+
+echo -e "\n\U0001f5bc${GREEN}\ticonic${ENDCOLOR}\n"
+yarn c check:types
+yarn c prebuild
+yarn c build
+
+echo -e "\n\U0001f3ba${GREEN}\torchestrator${ENDCOLOR}\n"
+yarn o check:types
+yarn o prebuild
+yarn o build
+
+echo -e "\n\t\u2728${GREEN}\tlayout${ENDCOLOR}\n"
+yarn l check:types
+yarn l build
