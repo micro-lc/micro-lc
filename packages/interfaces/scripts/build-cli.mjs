e76a6d85a77089846f1d03826918d0e82581053d
Umberto Toniolo <amountainram@gmail.com>
2022-10-20 14:32:49 +0200

    interfaces legal comments

--- packages/interfaces/scripts/build-cli.mjs
+++ packages/interfaces/scripts/build-cli.mjs
@@ -3,14 +3,20 @@ import { fileURLToPath } from 'url'
 
 import { build } from 'esbuild'
 
+import settings from '../../../settings.json'
+
 const __dirname = dirname(resolve(fileURLToPath(import.meta.url)))
 
 build({
+  banner: {
+    js: ['/*!', ...settings.banner, '*/'].join('\n'),
+  },
   bundle: true,
   entryPoints: [resolve(__dirname, '../cli/index.ts')],
   minify: true,
   outdir: resolve(__dirname, '../dist/cli'),
   platform: 'node',
+  target: settings.target,
 }).then(() => {
   console.log('\x1b[32m%s\x1b[0m', `✔ cli compiled successfully`)
 }).catch((err) => {
