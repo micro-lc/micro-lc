5de616c6eda217ab8844c482e7acb32adb2c5d0c
epessina <edoardopessina.priv@gmail.com>
2022-10-25 15:42:30 +0200

    Docs route base path

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -4,6 +4,7 @@ const lightCodeTheme = require('prism-react-renderer/themes/github')
 const pluginContentDocsConfig = {
   breadcrumbs: true,
   path: 'docs',
+  routeBasePath: '/',
   sidebarPath: 'sidebars.js',
 }
 
