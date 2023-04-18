1bd45fe266c1601dde3334fecf6d2376a71b57b4
Umberto Toniolo <amountainram@gmail.com>
2023-04-18 11:17:50 +0200

    Added polyfill
    a simple Symbol.observable polyfill is added
    altogether with a routine to bundle polyfills with micro-lc
    which does not alter the main src files
    
    globs are modified accordingly

---
+++ packages/orchestrator/scripts/build-polyfills.ts
@@ -0,0 +1,15 @@
+import { build } from 'esbuild'
+import { globSync } from 'glob'
+
+import settings from '../../../settings.json'
+
+const entryPoints = globSync('src/polyfills/**/*[^d].{j,t}s?(x)')
+  .filter((name) => !name.match(/(stories|test)\.(j|t)sx?$/))
+
+build({
+  entryPoints,
+  outdir: 'dist/polyfills',
+  target: settings.target,
+}).then(() => {
+  console.log('✓ polyfills')
+}).catch(console.error)
--- packages/orchestrator/scripts/glob.ts
+++ packages/orchestrator/scripts/glob.ts
@@ -2,3 +2,4 @@ import { globSync } from 'glob'
 
 export default globSync('src/**/*[^d].{j,t}s?(x)')
   .filter((name) => !name.match(/(stories|test)\.(j|t)sx?$/))
+  .filter((name) => !name.match(/src\/polyfills/))
