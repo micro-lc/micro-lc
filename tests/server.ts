892ca0181220260dc4a32cbf6e19332c81af2b73
Umberto Toniolo <amountainram@gmail.com>
2023-05-31 18:54:22 +0200

    bugfix on routing
    - routing towards a `parcel` application which is registered as `/app/` takes the precedence when calling the URL `/app`
    - public assets on a `parcel` application are served on a path which ends by a trailing slash
    - removed `console.log` statements

--- tests/server.ts
+++ tests/server.ts
@@ -1,12 +1,19 @@
-/* eslint-disable no-sync */
+
 import fs from 'fs'
 import path from 'path'
 
+import type { Response } from 'express'
 import express from 'express'
 
 const port = 3000
 
 const app = express()
+
+const notFound = (res: Response) => {
+  res.statusCode = 404
+  res.setHeader('Content-Type', 'application/json')
+  res.send({ error: 'Not Found', message: 'Not Found', status: 404 })
+}
 
 app.use('/playground', express.static('playground'))
 app.use('/packages', express.static('packages'))
@@ -15,36 +22,20 @@ app.use('/dev/*', express.static('tests/dev'))
 app.use('/pages/*', express.static('tests/pages'))
 app.use('/applications/*', express.static('tests/applications'))
 
-app.use('/__reverse/*', (req, res, next) => {
-  console.log(req.url)
-  const { url } = req
-  const baseIndex = '/__reverse/index.html'
-  const upperIndex = url.endsWith('/') ? path.join(url, 'index.html') : url.replace(/\/([^/]+)$/, '/index.html')
-  const entry = fs.lstatSync(path.join(__dirname, url), { throwIfNoEntry: false })
-  if (entry) {
-    req.url = upperIndex
-    express.static('/__reverse')(req, res, next)
-    return
-  }
+app.use('/__reverse/*', (_, res) => {
+  try {
+    fs.readFile(path.join(__dirname, '/__reverse/index.html'), (error, data) => {
+      if (error !== null) {
+        return notFound(res)
+      }
 
-  const upperIndexEntry = fs.lstatSync(path.join(__dirname, upperIndex), { throwIfNoEntry: false })
-  if (upperIndexEntry) {
-    req.url = upperIndex
-    express.static('/__reverse')(req, res, next)
-    return
+      res.statusCode = 200
+      res.setHeader('content-type', 'text/html')
+      res.send(data)
+    })
+  } catch (error) {
+    return notFound(res)
   }
-
-  const baseIndexEntry = fs.lstatSync(path.join(__dirname, baseIndex), { throwIfNoEntry: false })
-  if (baseIndexEntry) {
-    console.log('base')
-    req.url = baseIndex
-    express.static('/__reverse')(req, res, next)
-    return
-  }
-
-  res.statusCode = 404
-  res.setHeader('Content-Type', 'application/json')
-  res.send({ error: 'Not Found', message: `cannot find anything at ${url}`, status: 404 })
 })
 
 app.use('/configurations', (req, res) => {
