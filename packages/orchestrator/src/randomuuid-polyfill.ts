cdc9d19933561c4041c7895543430f31e27892cd
Paola Nicosia <paola.nicosia@mia-platform.eu>
2024-02-06 10:59:22 +0100

    chore(randomuuid-polyfill): fix linter error

--- packages/orchestrator/src/randomuuid-polyfill.ts
+++ packages/orchestrator/src/randomuuid-polyfill.ts
@@ -19,10 +19,11 @@ if (!('randomUUID' in window.crypto)) {
   // https://stackoverflow.com/a/2117523/2800218
   // LICENSE: https://creativecommons.org/licenses/by-sa/4.0/legalcode
   window.crypto.randomUUID = function randomUUID(): string {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
     return (
       // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
       [1e7] + -1e3 + -4e3 + -8e3 + -1e11
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     ).replace(/[018]/g,
       // eslint-disable-next-line no-mixed-operators
       (ch) => (ch ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> ch / 4).toString(16)
