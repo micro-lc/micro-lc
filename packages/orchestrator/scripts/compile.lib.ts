e7a0c8a50fd7bf9a3c716f9e359d8a53480c9594
Umberto Toniolo <amountainram@gmail.com>
2024-03-16 11:52:54 +0100

    feat(cjs): support cjs exports for mfe-loader lib

--- packages/orchestrator/scripts/compile.lib.ts
+++ packages/orchestrator/scripts/compile.lib.ts
@@ -18,6 +18,22 @@ build({
   console.log('✓ es module')
 }).then(async () => {
   await fs.cp('src/assets', 'dist/es/assets', { recursive: true })
-  console.log('✓ asset copied')
+  console.log('✓ asset copied to dist/es/assets')
+})
+  .catch(console.error)
+
+build({
+  banner: {
+    js: ['/*!', ...settings.banner, '*/'].join('\n'),
+  },
+  entryPoints,
+  format: 'cjs',
+  outdir: 'dist/cjs',
+  target: settings.target,
+}).then(() => {
+  console.log('✓ cjs module')
+}).then(async () => {
+  await fs.cp('src/assets', 'dist/cjs/assets', { recursive: true })
+  console.log('✓ asset copied to dist/cjs/assets')
 })
   .catch(console.error)
